616d3ed29946656a4b00b0e82fe5db16d5cbd975
Renasa <r.asanuma521@gmail.com>
2020-04-02 23:33:27 -0700

    Add css file

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
     
